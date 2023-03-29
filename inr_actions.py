0b54b51efa0843dc411d2de5b25f8bc2f96cc33f
rajni-persaud <rajnipersaud246@gmail.com>
2023-03-29 05:02:05 -0400

    added custom action: phrase_to_date

--- inr_actions.py
+++ inr_actions.py
@@ -1,5 +1,8 @@
 from jaseci.actions.live_actions import jaseci_action
 import re
+import dateparser
+from datetime import date
+from dateutil.relativedelta import relativedelta
 
 @jaseci_action(act_group=["inr"], allow_remote=True)
 def remove_html_tags(text: str):
@@ -12,3 +15,11 @@ def remove_html_tags(text: str):
     #remove html tags
     tags = re.compile('<.*?>')
     return tags.sub('', text)
+
+
+@jaseci_action(act_group=["inr"], allow_remote=True)
+def phrase_to_date(phrase: str):
+    _date = dateparser.parse(phrase)
+    if(_date):
+        return _date.strftime("%Y-%m-%d")
+    else: return (date.today()).strftime("%Y-%m-%d")
